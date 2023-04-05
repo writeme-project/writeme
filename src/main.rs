2aaf1224f1892ced5ffb429259adc53d120e01b5
BabelDev0 <leonardo.straccali@gmail.com>
2023-04-05 11:18:52 +0200

    wip (core): write README.md

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::prelude::*;
+use terminal_spinners::{ SpinnerBuilder, DOTS };
+use std::env;
+
+fn animate_loading() {
+    let handle = SpinnerBuilder::new().spinner(&DOTS).text(" Writing README.md").start();
+    let res =   write_readme();
+    match res {
+        Ok(_) => handle.done(),
+        Err(_) => handle.error(),
+    }
+}
+
+fn write_readme() -> std::io::Result<()> {
+    let mut file = File::create("README.MD")?;
+    file.write_all(b"# WRITEME")?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    animate_loading();
 }
