14ebdbf8629ba77cec35ebe6db727e134db15739
BabelDev0 <leonardo.straccali@gmail.com>
2023-04-08 13:18:47 +0200

    wip (core): merge info from different config object

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 use handlebars::Handlebars;
 use rand::prelude::*;
-use serde_json::{json, Value};
+use serde_json::{ Map, Value, json };
 use std::error::Error;
 #[allow(dead_code)]
 use std::fs;
 use std::fs::File;
-use terminal_spinners::{SpinnerBuilder, DOTS};
+use terminal_spinners::{ SpinnerBuilder, DOTS };
 
 const EMOJI_LIST: [&str; 16] = [
-    "🖋️", "📝", "📄", "📚", "📖", "📓", "📒", "📃", "📜", "📰", "📑", "🔖", "🔗", "📎", "📐", "📏",
+    "🖋️",
+    "📝",
+    "📄",
+    "📚",
+    "📖",
+    "📓",
+    "📒",
+    "📃",
+    "📜",
+    "📰",
+    "📑",
+    "🔖",
+    "🔗",
+    "📎",
+    "📐",
+    "📏",
 ];
 
 fn config_to_json(path: &str) -> Result<Value, Box<dyn Error>> {
-    let contents =
-        fs::read_to_string(path).expect("Should have been able to read the template file");
+    let contents = fs
+        ::read_to_string(path)
+        .expect("Should have been able to read the template file");
 
     let file_type = path.split(".").last().unwrap();
     match file_type {
@@ -27,13 +43,37 @@ fn config_to_json(path: &str) -> Result<Value, Box<dyn Error>> {
         }
         "toml" => {
             let json: Value = toml::from_str(contents.as_str()).unwrap();
-            print!("{}", serde_json::to_string_pretty(&json).unwrap());
+            // print!("{}", serde_json::to_string_pretty(&json).unwrap());
             return Ok(json);
         }
         _ => {
             return Err("File type not supported".into());
         }
     }
+}
+
+fn merge_json_objects(obj1: Value, obj2: Value) -> Value {
+    let obj1: Map<String, Value> = obj1.as_object().unwrap().clone();
+    let obj2: Map<String, Value> = obj2.as_object().unwrap().clone();
+
+    let mut merged = Map::new();
+    for (k, v) in obj1 {
+        merged.insert(k, v);
+    }
+    for (k, v) in obj2 {
+        let existing_val = merged.get(&k);
+        match existing_val {
+            Some(val) => {
+                if v.as_str().map_or(false, |s| s.len() > val.as_str().map_or(0, |v| v.len())) {
+                    merged.insert(k, v);
+                }
+            }
+            None => {
+                merged.insert(k, v);
+            }
+        }
+    }
+    return json!(merged);
 }
 
 fn random_emoji() -> String {
@@ -68,10 +108,7 @@ fn random_emoji() -> String {
 // }
 
 fn animate_loading() {
-    let handle = SpinnerBuilder::new()
-        .spinner(&DOTS)
-        .text(" Writing README.md")
-        .start();
+    let handle = SpinnerBuilder::new().spinner(&DOTS).text(" Writing README.md").start();
     let res = learn_about_project();
     match res {
         Ok(_) => handle.done(),
@@ -80,11 +117,17 @@ fn animate_loading() {
 }
 
 fn learn_about_project() -> Result<(), Box<dyn Error>> {
-    let mut config_json: Value = config_to_json("./assets/Cargo.toml")?;
+    let cargo: Value = config_to_json("./assets/Cargo.tpl.toml")?;
+    let package: Value = config_to_json("./assets/package_1.json")?;
 
-    config_json["icon"] = json!(random_emoji());
+    let mut config = merge_json_objects(cargo, package);
+    config["icon"] = json!(random_emoji());
 
-    writeme(config_json)?;
+    // write config to file
+    let fs = File::create("config.json")?;
+    serde_json::to_writer_pretty(fs, &config)?;
+
+    writeme(config)?;
 
     Ok(())
 }
@@ -92,9 +135,7 @@ fn learn_about_project() -> Result<(), Box<dyn Error>> {
 fn writeme(readme_contents: Value) -> Result<(), Box<dyn Error>> {
     let mut handlebars = Handlebars::new();
     let mut readme_file = File::create("README.MD")?;
-    handlebars
-        .register_template_file("template", "README.tpl.md")
-        .unwrap();
+    handlebars.register_template_file("template", "README.tpl.md").unwrap();
     handlebars.render_to_write("template", &readme_contents, &mut readme_file)?;
     Ok(())
 }
