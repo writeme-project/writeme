7a1d78c524c889e5a71a53275c72dd9766ac8193
ilcors-dev <corsetti.luca28@gmail.com>
2023-04-11 23:26:23 +0200

    feat: wip #5

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,19 @@
 use handlebars::Handlebars;
 use rand::prelude::*;
-use serde_json::{ Map, Value, json };
-use std::{ error::Error };
+use serde_json::{json, Map, Value};
+use std::error::Error;
 #[allow(dead_code)]
 use std::fs;
 use std::fs::File;
-use terminal_spinners::{ SpinnerBuilder, DOTS };
+use terminal_spinners::{SpinnerBuilder, DOTS};
 
 const EMOJI_LIST: [&str; 16] = [
-    "🖋️",
-    "📝",
-    "📄",
-    "📚",
-    "📖",
-    "📓",
-    "📒",
-    "📃",
-    "📜",
-    "📰",
-    "📑",
-    "🔖",
-    "🔗",
-    "📎",
-    "📐",
-    "📏",
+    "🖋️", "📝", "📄", "📚", "📖", "📓", "📒", "📃", "📜", "📰", "📑", "🔖", "🔗", "📎", "📐", "📏",
 ];
 
 fn config_to_json(path: &str) -> Result<Value, Box<dyn Error>> {
-    let contents = fs
-        ::read_to_string(path)
-        .expect("Should have been able to read the template file");
+    let contents =
+        fs::read_to_string(path).expect("Should have been able to read the template file");
 
     let file_type = path.split(".").last().unwrap();
     match file_type {
@@ -42,7 +26,15 @@ fn config_to_json(path: &str) -> Result<Value, Box<dyn Error>> {
             return Ok(json);
         }
         "toml" => {
-            let json: Value = toml::from_str(contents.as_str()).unwrap();
+            let mut json: Value = toml::from_str(contents.as_str()).unwrap();
+
+            // flatten [package] category
+            for (key, value) in json["package"].clone().as_object().unwrap().iter() {
+                json[key] = value.clone();
+            }
+
+            json.as_object_mut().unwrap().remove("package");
+
             // print!("{}", serde_json::to_string_pretty(&json).unwrap());
             return Ok(json);
         }
@@ -52,20 +44,36 @@ fn config_to_json(path: &str) -> Result<Value, Box<dyn Error>> {
     }
 }
 
-fn merge_json_objects(obj1: Value, obj2: Value) -> Value {
+fn merge_json_objects(obj1: Value, obj2: Value) -> Result<Value, Box<dyn Error>> {
     let obj1: Map<String, Value> = obj1.as_object().unwrap().clone();
     let obj2: Map<String, Value> = obj2.as_object().unwrap().clone();
 
     let mut merged = Map::new();
+
     for (k, v) in obj1 {
         merged.insert(k, v);
     }
+
     for (k, v) in obj2 {
         let existing_val = merged.get(&k);
+
         match existing_val {
             Some(val) => {
-                if v.as_str().map_or(false, |s| s.len() > val.as_str().map_or(0, |v| v.len())) {
-                    merged.insert(k, v);
+                let conflicts = [val.to_string(), v.to_string()];
+
+                dialoguer::console::Term::stdout()
+                    .write_line(format!("Found conflicting key: {k}").as_str())
+                    .unwrap();
+
+                let selection = dialoguer::Select::new()
+                    .with_prompt("Which value would you like to keep?")
+                    .items(&conflicts)
+                    .default(0)
+                    .interact_on_opt(&dialoguer::console::Term::stderr())?;
+
+                match selection {
+                    Some(index) => println!("User selected item : {}", conflicts[index]),
+                    None => println!("User did not select anything"),
                 }
             }
             None => {
@@ -73,7 +81,8 @@ fn merge_json_objects(obj1: Value, obj2: Value) -> Value {
             }
         }
     }
-    return json!(merged);
+
+    return Ok(json!(merged));
 }
 
 fn random_emoji() -> String {
@@ -100,9 +109,10 @@ impl Shield {
         let shield_tpl = fs::read_to_string("./src/tpl/SHIELD.md").unwrap();
         // use handlebars to replace placeholders with values
         let mut handlebars = Handlebars::new();
-        handlebars.register_template_string("shield_tpl", shield_tpl.clone()).unwrap();
-        let data =
-            json!({
+        handlebars
+            .register_template_string("shield_tpl", shield_tpl.clone())
+            .unwrap();
+        let data = json!({
             "label": self.label,
             "message": self.message,
             "color": self.color,
@@ -119,20 +129,17 @@ impl Shield {
     }
 }
 
-fn animate_loading() {
-    let handle = SpinnerBuilder::new().spinner(&DOTS).text(" Writing README.md").start();
-    let res = learn_about_project();
-    match res {
-        Ok(_) => handle.done(),
-        Err(_) => handle.error(),
-    }
-}
-
 fn learn_about_project() -> Result<(), Box<dyn Error>> {
     let cargo: Value = config_to_json("./assets/Cargo.tpl.toml")?;
     let package: Value = config_to_json("./assets/package_1.json")?;
 
-    let mut config = merge_json_objects(cargo, package);
+    let mut config = match merge_json_objects(cargo, package) {
+        Ok(merged) => merged,
+        Err(_) => {
+            return Err("Failed to merge JSON objects".into());
+        }
+    };
+
     config["icon"] = json!(random_emoji());
 
     // write config to file
@@ -147,11 +154,23 @@ fn learn_about_project() -> Result<(), Box<dyn Error>> {
 fn writeme(readme_contents: Value) -> Result<(), Box<dyn Error>> {
     let mut handlebars = Handlebars::new();
     let mut readme_file = File::create("README.MD")?;
-    handlebars.register_template_file("template", "README.tpl.md").unwrap();
+    handlebars
+        .register_template_file("template", "README.tpl.md")
+        .unwrap();
     handlebars.render_to_write("template", &readme_contents, &mut readme_file)?;
     Ok(())
 }
 
 fn main() {
-    animate_loading();
+    let handle = SpinnerBuilder::new()
+        .spinner(&DOTS)
+        .text(" Writing README.md")
+        .start();
+
+    let res = learn_about_project();
+
+    match res {
+        Ok(_) => handle.done(),
+        Err(_) => handle.error(),
+    }
 }
