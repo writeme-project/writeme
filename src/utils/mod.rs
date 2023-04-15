d6d0c215ba9d7f36f1d604c8db2e06ef0be45f97
BabelDev0 <leonardo.straccali@gmail.com>
2023-04-15 22:35:22 +0200

    feat (structure): added mod utils to store path of utils file

---
+++ src/utils/mod.rs
@@ -0,0 +1,7 @@
+pub mod paths {
+    pub const CONFIGS: &str = "./src/configs.yml";
+    pub const TEMPLATE: &str = "./assets/TEMPLATE.md";
+    pub const SHIELD: &str = "./assets/SHIELD.md";
+    pub const README: &str = "./README.md";
+    pub const TECHS: &str = "./src/techs.json";
+}
