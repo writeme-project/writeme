ec6b0f39b609baa4b00a7c35a0eba895ed11ffa7
BabelDev0 <leonardo.straccali@gmail.com>
2023-04-20 14:24:15 +0200

    fix (core): wrong files path

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -5,8 +5,8 @@ use std::fs;
 
 pub mod paths {
     pub const CONFIGS: &str = "./src/configs.yml";
-    pub const TEMPLATE: &str = "./assets/TEMPLATE.md";
-    pub const SHIELD: &str = "./assets/SHIELD.md";
+    pub const TEMPLATE: &str = "./assets/tpl/TEMPLATE.md";
+    pub const SHIELD: &str = "./assets/tpl/SHIELD.md";
     pub const README: &str = "./README.md";
     pub const TECHS: &str = "./src/techs.yml";
 }
