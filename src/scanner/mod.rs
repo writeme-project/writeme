a64cda101391bd7e3fdd0519ae8f1ef99642e52f
BabelDev0 <leonardo.straccali@gmail.com>
2023-04-30 14:23:45 +0200

    feat(scanner): replace glob with rust_search

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -1,39 +1,41 @@
-use crate::utils::{paths, Tech};
+use crate::utils::{paths, Project, Tech};
 use anyhow::Error;
 use std::{
     collections::HashMap,
     fs::{self},
+    vec,
 };
 
 // Returns list of config files present in the project
-pub fn scan_configs(project_location: &str) -> Result<Vec<String>, Error> {
-    let contents =
+pub fn scan_configs(paths: &Vec<String>) -> Result<Vec<String>, Error> {
+    let contents: String =
         fs::read_to_string(paths::CONFIGS).expect("Something went wrong reading the config file");
     let all_configs: HashMap<String, Vec<String>> = serde_yaml::from_str(&contents).unwrap();
     let all_configs: Vec<String> = all_configs
         .values()
         .flatten()
-        .map(|c| format!(r"{}$", c))
+        .map(|c: &String| format!(r"{}$", c))
         .collect();
 
-    let regex_set = regex::RegexSet::new(all_configs).unwrap();
-
-    let paths = glob::glob(format!("{}/**/*", project_location).as_str()).unwrap();
+    let regex_set: regex::RegexSet = regex::RegexSet::new(all_configs).unwrap();
 
     let mut configs_present: Vec<String> = vec![];
+
     for path in paths {
-        let path = path.unwrap();
-        let path_str = path.to_str().unwrap();
+        let path_str = path.as_str();
         let matches: Vec<_> = regex_set.matches(path_str).into_iter().collect();
         if !matches.is_empty() {
             configs_present.push(path_str.to_string());
         }
     }
+
+    println!("Questa è la lista dei config files {:?}", configs_present);
+
     Ok(configs_present)
 }
 
 // Returns the list of shield urls for the technologies found through the config files
-pub fn scan_techs() -> Result<Vec<String>, Error> {
+pub fn scan_techs(paths: &Vec<String>) -> Result<Vec<String>, Error> {
     let contents: String =
         fs::read_to_string(paths::TECHS).expect("Something went wrong reading the techs file");
     let all_techs: HashMap<String, Tech> = serde_yaml::from_str(&contents).unwrap();
@@ -46,10 +48,8 @@ pub fn scan_techs() -> Result<Vec<String>, Error> {
         }
         let regex_set = regex::RegexSet::new(tech.config_files).unwrap();
 
-        let paths = glob::glob("./**/*").unwrap();
         for path in paths {
-            let path = path.unwrap();
-            let path_str = path.to_str().unwrap();
+            let path_str = path.as_str();
             let matches: Vec<_> = regex_set.matches(path_str).into_iter().collect();
 
             if !matches.is_empty() {
