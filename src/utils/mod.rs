3a19cbf2897e25ecb18e9597eb878987f7ce35ea
BabelDev0 <leonardo.straccali@gmail.com>
2023-05-26 10:49:08 +0200

    feat(tpl): restyle and adde fantasy description

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,6 @@
 use anyhow::Error;
 use handlebars::Handlebars;
+use rand::seq::SliceRandom;
 use rust_search::{FilterExt, SearchBuilder};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
@@ -149,4 +150,57 @@ impl Project {
 
         Ok(Project { paths })
     }
+}
+
+// random fantasy descriptions
+const DESCRIPTIONS: [&str; 42] = [
+    "a mystical realm where imagination reigns supreme",
+    "a celestial dance of colors and melodies",
+    "an enchanted forest of boundless creativity",
+    "a shimmering star guiding us towards innovation",
+    "a symphony of brilliant minds harmonizing together",
+    "a phoenix rising from the ashes of conventional thinking",
+    "a magical tapestry of art, science, and wonder",
+    "a breathtaking voyage through uncharted territories",
+    "an ethereal sanctuary where imagination knows no bounds",
+    "a kaleidoscope of possibilities waiting to be explored",
+    "a luminous beacon of inspiration in a world of shadows",
+    "a majestic dragon breathing life into daring ideas",
+    "a secret garden where dreams take root and flourish",
+    "a mystical portal to realms yet undiscovered",
+    "an alchemical fusion of passion, vision, and brilliance",
+    "a sanctuary where innovation and creativity intertwine",
+    "an ever-evolving tapestry of boundless imagination",
+    "a cosmic birthplace of revolutionary concepts",
+    "a captivating symphony of technology and artistry",
+    "a celestial masterpiece painted on the canvas of possibility",
+    "a project that makes unicorns jealous",
+    "the Gandalf of innovation, uttering \"You shall not stagnate!\"",
+    "like a magical potion that turns ideas into gold",
+    "the Chuck Norris of creativity, delivering roundhouse kicks to the mundane",
+    "a project so epic, it could defeat the One Ring",
+    "the secret ingredient in the recipe for mind-blowing awesomeness",
+    "the Batman of projects, silently saving the world from mediocrity",
+    "the Queen Bey of innovation, slaying it on every stage",
+    "the project equivalent of a double rainbow, but with unicorns riding skateboards",
+    "the Elon Musk of ideas, launching us into a future we never imagined",
+    "a project that makes even cats say, \"I can haz innovation?\"",
+    "the project equivalent of a disco ball, making everything sparkle with ingenuity",
+    "the project that can turn \"undefined\" into \"defined\" with a single line of code",
+    "like a regex wizard, matching innovation patterns with supernatural accuracy",
+    "the project that can refactor spaghetti code into a gourmet lasagna of elegance",
+    "like a CSS sorcerer, magically styling innovation with pixel-perfect precision",
+    "the project that can conquer infinite loops with the power of infinite recursion",
+    "the project that can optimize algorithms faster than the Flash on an espresso high",
+    "the project that can debug issues with the ease of Neo dodging bullets in The Matrix",
+    "the project that can deploy to production so seamlessly, it makes NASA's rocket launches ",
+    "the project that can handle more data than a black hole can handle gravity",
+    "the project that can refactor legacy code so effectively, it makes Dumbledore's wand ",
+];
+
+// use to generate a random fantasy description for the project
+pub fn fantasy_description() -> String {
+    let mut rng = rand::thread_rng();
+    let fantasy_description = *DESCRIPTIONS.choose(&mut rng).unwrap();
+    fantasy_description.to_string()
 }
