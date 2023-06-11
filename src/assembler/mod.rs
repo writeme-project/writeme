5781038ffa458250fc00208cf66504874759c239
ilcors-dev <corsetti.luca28@gmail.com>
2023-06-11 19:25:47 +0200

    feat: License #13

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,5 +1,6 @@
 use crate::{
     converter::{ConverterOutput, Repository, RepositoryPlatform},
+    dialoguer::{select_option, SelectOption},
     scanner::{self, scan_dependencies, scan_techs},
     utils::{fantasy_description, paths, shields, Aligment, GenMarkdown},
 };
@@ -9,6 +10,29 @@ use handlebars::Handlebars;
 use serde_json::{json, Value};
 use std::fs::File;
 use std::io::Write;
+
+/// The available licenses for a project which a user can choose from
+pub enum License {
+    MIT,
+    Apache2,
+    GPL3,
+    BSD3,
+    Unlicense,
+    Custom(String),
+}
+
+impl ToString for License {
+    fn to_string(&self) -> String {
+        match self {
+            License::MIT => "MIT".to_string(),
+            License::Apache2 => "Apache-2.0".to_string(),
+            License::GPL3 => "GPL-3.0".to_string(),
+            License::BSD3 => "BSD-3-Clause".to_string(),
+            License::Unlicense => "Unlicense".to_string(),
+            License::Custom(custom_license) => custom_license.clone(),
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct Assembler<'a> {
@@ -59,7 +83,7 @@ impl<'a> Assembler<'a> {
     /// - license set in config files but not found in the project
     /// - no license file nor license set in config files, ask the user which license to use
     fn assemble_license(&mut self, paths: &Vec<String>) -> String {
-        let license = match scanner::find_license_file(paths) {
+        let mut license = match scanner::find_license_file(paths) {
             Ok(license_path) => {
                 let license_tpl = paths::read_util_file_contents(paths::UtilityPath::License);
 
@@ -84,7 +108,7 @@ impl<'a> Assembler<'a> {
                     };
 
                     license = json!({
-                        "name": "see here for more details",
+                        "name": "Available here",
                         "target": repository.url.clone() + "/blob/master/" + without_project_path.as_str(),
                     });
                 } else {
@@ -93,16 +117,42 @@ impl<'a> Assembler<'a> {
                     });
                 };
 
-                self.handlebars.render("license_tpl", &license).unwrap()
+                Some(self.handlebars.render("license_tpl", &license).unwrap())
             }
-            Err(_) => self
-                .converted_config
-                .license
-                .clone()
-                .unwrap_or("Not found".to_string()),
+            Err(_) => None,
         };
 
-        license
+        // no license file found, check if license is set in config files
+        if license.is_none() && self.converted_config.license.is_some() {
+            license = self.converted_config.license.clone();
+        }
+
+        // no license file found and no license set in config files, ask the user which license to use
+        if license.is_none() {
+            let available = vec![
+                License::MIT.to_string(),
+                License::Apache2.to_string(),
+                License::GPL3.to_string(),
+                License::BSD3.to_string(),
+                License::Unlicense.to_string(),
+                License::Custom("Custom".to_string()).to_string(),
+            ]
+            .iter()
+            .map(|license| SelectOption {
+                name: license.clone(),
+                value: Some(license.clone()),
+            })
+            .collect();
+
+            license = select_option(
+                "LICENSE",
+                available,
+                Some("I was unable to find a license in your project! Select one from the list below"
+                .to_string()),
+            );
+        }
+
+        license.unwrap_or(License::Unlicense.to_string())
     }
 
     fn assemble_body(&mut self, paths: &Vec<String>) -> String {
